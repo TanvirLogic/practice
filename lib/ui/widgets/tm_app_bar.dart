0a5a96708422e1756f90e071d630a5a91766d7d8
assignment <bmwthriad2023@gmail.com>
2025-11-09 16:18:24 +0600

    running last part of UI

--- lib/ui/widgets/tm_app_bar.dart
+++ lib/ui/widgets/tm_app_bar.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:practice/ui/screens/update_profile_screen.dart';
+
 class TMAppBar extends StatelessWidget implements PreferredSizeWidget {
   const TMAppBar({super.key});
 
@@ -6,18 +8,26 @@ class TMAppBar extends StatelessWidget implements PreferredSizeWidget {
   Widget build(BuildContext context) {
     return AppBar(
       backgroundColor: Colors.green,
-      title: Row(
-        children: [
-          CircleAvatar(),
-          SizedBox(width: 10),
-          Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Text('Md Tanvir Ahmed', style: TextStyle(fontSize: 18)),
-              Text('bmwthriad2023@gmail.com', style: TextStyle(fontSize: 14)),
-            ],
-          ),
-        ],
+      title: GestureDetector(
+        onTap: () {
+          Navigator.push(
+            context,
+            MaterialPageRoute(builder: (context) => UpdateProfileScreen()),
+          );
+        },
+        child: Row(
+          children: [
+            CircleAvatar(),
+            SizedBox(width: 10),
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text('Md Tanvir Ahmed', style: TextStyle(fontSize: 18)),
+                Text('bmwthriad2023@gmail.com', style: TextStyle(fontSize: 14)),
+              ],
+            ),
+          ],
+        ),
       ),
       actions: [IconButton(onPressed: () {}, icon: Icon(Icons.logout))],
     );
